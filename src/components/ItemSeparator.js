96e603eac4ab5f6776e33e3aa6bf77e81995f165
Emilio Martin <emiunrn@gmail.com>
2022-06-04 17:46:03 -0300

    test popular movies list

---
+++ src/components/ItemSeparator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ItemSeparator = () => {
+  return <View /* style={{height: 1, marginHorizontal: 10}} */></View>;
+};
+
+export default ItemSeparator;
---
+++ src/components/Test.js
@@ -0,0 +1,61 @@
+import {SafeAreaView, FlatList, TouchableOpacity, Text} from 'react-native';
+import ItemSeparator from './ItemSeparator';
+import React, {useState, useEffect} from 'react';
+import * as env from '../environment/development';
+
+const Test = () => {
+  const [movies, setMovies] = useState([]);
+  const [config, setConfig] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchConfig();
+    fetchMovies();
+  }, []);
+
+  function fetchMovies() {
+    setError(null);
+    setLoading(true);
+    setMovies([]);
+
+    fetch(env.API_URL + '/movie/popular?api_key=' + env.API_KEY)
+      .then(response => response.json())
+      .then(json => {
+        setMovies(json.results);
+        setLoading(false);
+      });
+  }
+
+  function fetchConfig() {
+    fetch(env.CONFIGURATION_URL + env.API_KEY)
+      .then(response => response.json())
+      .then(json => setConfig(json));
+  }
+
+  function handleRefresh() {
+    fetchMovies();
+  }
+
+  const renderItemComponent = movie => (
+    <TouchableOpacity>
+      <Text>{movie.title}</Text>
+      <Text>{movie.release_date}</Text>
+      <Text>{movie.vote_average}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={movies}
+        renderItem={movie => renderItemComponent(movie)}
+        keyExtractor={movie => movie.id}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        refreshing={loading}
+        onRefresh={handleRefresh}></FlatList>
+    </SafeAreaView>
+  );
+};
+
+export default Test;
