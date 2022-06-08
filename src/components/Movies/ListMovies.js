9aff8c82591f67a7aeff500c3029e2c3ec4e37d8
Emilio Martin <emiunrn@gmail.com>
2022-06-07 21:51:19 -0300

    header movie list to filter

--- src/components/Movies/ListMovies.js
+++ src/components/Movies/ListMovies.js
@@ -3,9 +3,11 @@ import ItemSeparator from '../ItemSeparator';
 import React, {useState, useEffect} from 'react';
 import * as env from '../../environment/development';
 import Movie from './Movie';
+import HeaderMovieFilter from './HeaderMovieFilter';
 
 const ListMovies = () => {
   const [movies, setMovies] = useState([]);
+  const [moviesFilter, setMoviesFilter] = useState(env.MOVIES_PATH[0]);
   const [config, setConfig] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -13,14 +15,14 @@ const ListMovies = () => {
   useEffect(() => {
     fetchConfig();
     fetchMovies();
-  }, []);
+  }, [moviesFilter]);
 
   function fetchMovies() {
     setError(null);
     setLoading(true);
     setMovies([]);
 
-    fetch(env.API_URL + '/movie/popular?api_key=' + env.API_KEY)
+    fetch(env.API_URL + moviesFilter.url + '?api_key=' + env.API_KEY)
       .then(response => response.json())
       .then(json => {
         setMovies(json.results);
@@ -42,6 +44,10 @@ const ListMovies = () => {
     <Movie movie={movie} baseUrl={config.images.base_url} />
   );
 
+  const renderHeaderComponent = (setFilter, actualFilter) => (
+    <HeaderMovieFilter setFilter={setFilter} actualFilter={actualFilter} />
+  );
+
   return (
     <SafeAreaView>
       <FlatList
@@ -50,7 +56,12 @@ const ListMovies = () => {
         keyExtractor={movie => movie.id}
         ItemSeparatorComponent={() => <ItemSeparator />}
         refreshing={loading}
-        onRefresh={handleRefresh}></FlatList>
+        onRefresh={handleRefresh}
+        ListHeaderComponent={() =>
+          renderHeaderComponent(setMoviesFilter, moviesFilter)
+        }
+        stickyHeaderIndices={[0]}
+        stickyHeaderHiddenOnScroll={true}></FlatList>
     </SafeAreaView>
   );
 };
